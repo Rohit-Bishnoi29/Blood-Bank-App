634d805f73c721fc25ca4af0894320830163656d
Rohit-Bishnoi29 <rohit_12013099@nitkkr.ac.in>
2023-07-03 12:06:00 +0530

    authentication APIs

--- server.js
+++ server.js
@@ -4,13 +4,19 @@ const colors = require('colors');
 const morgan = require('morgan');
 const cors = require('cors');
 const connectDB = require('./config/db');
+//configs
 dotenv.config();
 connectDB();
+// app setup
 const app = express();
-const PORT = process.env.PORT||8080
+const PORT = process.env.PORT || 8080
+//middlewares
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(cors());
-app.use("/api/v1/test",require("./routes/testRoute"));
-app.listen(PORT,()=>{
-    console.log(`server is running in ${process.env.DEV_MODE} on port ${PORT} `)})
+// routes
+app.use("/api/v1/auth", require("./routes/authRoutes"));
+app.use("/api/v1/test", require("./routes/testRoute"));
+app.listen(PORT, () => {
+    console.log(`server is running in ${process.env.DEV_MODE} on port ${PORT} `)
+})
